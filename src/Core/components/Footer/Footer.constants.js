8560e0bc883441b266ee99ed0d197e2c1743486a
Connor Keenum <nellakprime@proton.me>
2025-11-27 10:57:11 -0600

    Refactor footer to style standards.

---
+++ src/Core/components/Footer/Footer.constants.js
@@ -0,0 +1,8 @@
+export const LOGO_DESCRIPTION = 'Our solutions make production faster and cheaper. Contact us for more information.'
+export const FOOTER_COLUMN_DATA = [
+    { title: 'Company', links: [{ label: 'About us' }, { label: 'Customers' }, { label: 'Newsroom' }, { label: 'Events' }] },
+    { title: 'Industries', links: [{ label: 'Precision Metaforming' }, { label: 'Industrial Manufacturing' }, { label: 'High Tech & Electronics' }, { label: 'Aerospace' },] },
+    { title: 'Products', links: [{ label: 'Manufacturing Execution System' }, { label: 'Enterprise Resource Planning' }, { label: 'Quality Management System' }, { label: 'Supply Chain Planning' },] },
+    { title: 'Get in Touch', links: [{ label: 'example@example.com' }] },
+] // If this was not constant it would had been in a special hook
+export const CLOSING_REMARKS = { left: '&#169; 2025 Prodmast. All rights reserved', right: 'Terms & Conditions' }
---
+++ src/UI/molecules/Footer/Footer.slots.js
@@ -0,0 +1,33 @@
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import Typography from '@mui/material/Typography'
+import NextLink from 'next/link'
+import { LeftSlot } from '../Nav/Nav.slots'
+import { MAX_CONTENT_WIDTH } from '@/Core/shared/global.constants'
+import { FOOTER_COLUMN_DATA, LOGO_DESCRIPTION, CLOSING_REMARKS } from '@/Core/components/Footer/Footer.constants'
+// TODO: Fix the inconsistent gap between columns on < sm screens when it is in flex-direction: column
+const { left, right } = CLOSING_REMARKS
+const Column = ({ state }) => (
+    <Box height='100%' display='flex' flexDirection='column' gap={2} >
+        <Typography component='h3' variant='h3'>{state?.title}</Typography>
+        <Box display='flex' flexDirection='column' gap={2}>
+            {state?.links?.map(({ href, label }) => (<NextLink key={label} href={href ?? '/'}>{label}</NextLink>))}
+        </Box>
+    </Box>
+)
+export const DefaultChildren = (
+    <Box display='flex' flexDirection='column' px={3} alignItems='center' sx={theme => ({ backgroundColor: theme.palette.background.paperBackground })}>
+        <Box display='flex' justifyContent='space-between' alignItems={{ xs: 'center', sm: 'flex-start' }} flexDirection={{ xs: 'column', sm: 'row' }} gap={{ xs: 6, sm: 3 }} my={4} mx='0px' px={3} maxWidth={MAX_CONTENT_WIDTH} width='100%'>
+            <Box display='flex' flexDirection='column' minWidth='20%' maxWidth={{ xs: '100%', sm: '200px' }} width={{ xs: '50%', sm: 'fit-content' }}>
+                <LeftSlot /><Typography variant='body2'>{LOGO_DESCRIPTION}</Typography>
+            </Box>
+            <Box display='flex' justifyContent='space-between' alignItems='flex-start' flexDirection={{ xs: 'column', sm: 'row' }} width={{ xs: '50%', sm: '100%' }} flexWrap='wrap' gap={6}>
+                {FOOTER_COLUMN_DATA?.map(state => <Column key={state?.title} state={state} />)}
+            </Box>
+        </Box>
+        <Divider width='100%' sx={{ maxWidth: MAX_CONTENT_WIDTH }} />
+        <Box minHeight='96px' display='flex' alignItems='center' justifyContent={{ xs: 'center', sm: 'space-between' }} flexDirection={{ xs: 'column', sm: 'row' }} maxWidth={MAX_CONTENT_WIDTH} width='100%' gap={3} p={3}>
+            <Typography>{left}</Typography><Typography>{right}</Typography>
+        </Box>
+    </Box>
+)
