f93211988af057d8f02f458daae279495638c173
Connor Keenum <nellakprime@proton.me>
2025-11-29 16:40:48 -0600

    Make HomeSecond accessible and semantic.

--- src/UI/molecules/VerticalSection/HomeSecond/HomeSecond.helper.slots.js
+++ src/UI/molecules/VerticalSection/HomeSecond/HomeSecond.helper.slots.js
@@ -2,9 +2,10 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { LuArrowUpRight } from 'react-icons/lu'
 import { ICON_SIZE } from '@/Core/shared/global.constants'
-export const StandardIcon = ({ Icon }) => (<Box width={ICON_SIZE} height={ICON_SIZE} sx={theme => ({ '&:hover': { cursor: 'pointer', 'path': { color: theme.palette.primary.main } } })}>{<Icon size={ICON_SIZE} />}</Box>)
+export const StandardIcon = ({ Icon }) => (<Box width={ICON_SIZE} height={ICON_SIZE}>{<Icon size={ICON_SIZE} aria-hidden />}</Box>)
 export const GridItem = ({ children }) => (
     <Box
+        component='li'
         minWidth={0}
         height='100%'
         display='flex'
@@ -15,9 +16,7 @@ export const GridItem = ({ children }) => (
         p={4}
         borderRadius={2}
         overflow='hidden'
-        sx={theme => ({
-            backgroundColor: theme.palette.background.paper
-        })}
+        sx={theme => ({ backgroundColor: theme.palette.background.paper })}
     >
         {children}
     </Box>
@@ -25,7 +24,7 @@ export const GridItem = ({ children }) => (
 export const TopGridItem = ({ Icon }) => (<Box display='flex' alignItems='center' justifyContent='space-between'><StandardIcon Icon={Icon} /><StandardIcon Icon={LuArrowUpRight} /></Box>)
 export const BottomGridItem = ({ heading, description }) => (
     <Box display='flex' flexDirection='column' alignItems='flex-start' gap={3}>
-        <Typography textAlign='start' sx={theme => ({ fontSize: { xs: theme.typography.h4.fontSize, sm: theme.typography.h3.fontSize }, fontWeight: 'bold' })}>{heading}</Typography>
+        <Typography variant='h3' textAlign='start' sx={theme => ({ fontSize: { xs: theme.typography.h4.fontSize, sm: theme.typography.h3.fontSize }, fontWeight: 'bold' })}>{heading}</Typography>
         <Typography textAlign='start' sx={theme => ({ fontSize: { xs: theme.typography.body2.fontSize, sm: theme.typography.body1.fontSize, } })}>{description}</Typography>
     </Box>
 )
--- src/UI/molecules/VerticalSection/HomeSecond/HomeSecond.index.slots.js
+++ src/UI/molecules/VerticalSection/HomeSecond/HomeSecond.index.slots.js
@@ -5,19 +5,18 @@ import { HOME_SECOND_DATA, SECTION_HEADER } from '@/Core/components/VerticalSect
 import { GridItem, TopGridItem, BottomGridItem } from './HomeSecond.helper.slots'
 
 // TODO: Fix ugly colors and the MUITheme palette in general to something better
-// TODO: Fix aria stuff
-// TODO: Fix semantic stuff
 // TODO: Use the Typography default override instead of repeating long sx stuff
 // TODO: Extract the Header/Sub-header typography into its own re-used molecule, it is also used in HomePayment
 const { header, subHeader } = SECTION_HEADER
 export const DefaultChildrenHomeSecond = (
     <Box display='flex' flexDirection='column' alignItems='center' p={{ xs: 0, sm: 3 }} gap={3} maxWidth={MAX_CONTENT_WIDTH} width={{ xs: '70%', sm: '100%' }}>
-        <Box display='flex' flexDirection='column' alignItems='center' width={{ xs: '100%', sm: '50%' }} gap={3} >
-            <Typography width='100%' variant='h2' fontWeight='bold' textAlign='center' sx={theme => ({ fontSize: { xs: theme.typography.h3.fontSize, md: theme.typography.h2.fontSize }, color: theme.palette.primary.contrastText })}>{header}</Typography>
+        <Box component='header' aria-labelledby='home-second-header' display='flex' flexDirection='column' alignItems='center' width={{ xs: '100%', sm: '50%' }} gap={3} >
+            <Typography id='home-second-header' width='100%' variant='h2' fontWeight='bold' textAlign='center' sx={theme => ({ fontSize: { xs: theme.typography.h3.fontSize, md: theme.typography.h2.fontSize }, color: theme.palette.primary.contrastText })}>{header}</Typography>
             <Typography width='70%' textAlign='center' sx={theme => ({ fontSize: { xs: theme.typography.body2.fontSize, md: theme.typography.body1.fontSize }, color: theme.palette.primary.contrastText })}>{subHeader}</Typography>
         </Box>
         <Box
             aria-label='home-grid-region'
+            component='ul'
             display='grid'
             gap={3}
             width='100%'
