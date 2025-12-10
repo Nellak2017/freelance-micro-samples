2cb1bec4f0445595e154970c2275c20d32714e70
Connor Keenum <nellakprime@proton.me>
2025-12-10 17:56:23 -0600

    Fix typo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import { muiGlobalStyles } from '@/UI/styles/globalStyles'
 import { useThemeMode } from '@/Application/hooks/shared/useThemeMode'
 import { AppCacheProvider } from '@mui/material-nextjs/v16-pagesRouter'
 import createEmotionCache from '@/Core/shared/createEmotionCache'
-import { Analytics } from 'vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 const clientCache = createEmotionCache({ key: 'css', enableCssLayer: false })
 const App = ({ Component, pageProps }) => {
     const { theme } = useThemeMode()
